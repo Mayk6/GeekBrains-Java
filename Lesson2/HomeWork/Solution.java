baa433fe264948050a60ddbab99673fe00601d42
Ilia <mayksik@gmail.com>
2022-12-13 15:14:29 +0300

    Add Class

--- Lesson2/HomeWork/Solution.java
+++ Lesson2/HomeWork/Solution.java
@@ -1,4 +1,13 @@
+
 class Solution {
+
+
+
+
+public static void main(String[]args){
+
+        }
+
 //    Task 1
 //    You are given an m x n binary matrix grid. An island is a group of 1's (representing land)
 //    connected 4-directionally (horizontal or vertical.
